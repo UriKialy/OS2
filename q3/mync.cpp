c4b4a8409adcb1c4cbad505186dc456377bd24c6
urikialy <uri.kialy@msmail.ariel.ac.il>
2024-05-26 21:46:09 +0300

    add comments

--- q3/mync.cpp
+++ q3/mync.cpp
@@ -38,31 +38,31 @@ void handle_client_output(int client_sock) {
 
 // Function to start a TCP server
 int start_tcp_server(const string &port) {
-    int server_sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_sock < 0) {
+    int server_sock = socket(AF_INET, SOCK_STREAM, 0);// create a socket if error handle it
+    if (server_sock < 0) {// handle error
         perror("Error creating socket");
         exit(EXIT_FAILURE);
     }
 
     int opt = 1;
-    if (setsockopt(server_sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
+    if (setsockopt(server_sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {// set socket options if error handle it
         perror("Error setting socket options");
         close(server_sock);
         exit(EXIT_FAILURE);
     }
-
+    // set the server options
     struct sockaddr_in server_addr = {};
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(stoi(port));
     server_addr.sin_addr.s_addr = INADDR_ANY;
 
-    if (bind(server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+    if (bind(server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) { // bind the socket if error handle it
         perror("Error binding socket");
         close(server_sock);
         exit(EXIT_FAILURE);
     }
 
-    if (listen(server_sock, 1) < 0) {
+    if (listen(server_sock, 1) < 0) { // listen on the socket if error handle it
         perror("Error listening on socket");
         close(server_sock);
         exit(EXIT_FAILURE);
@@ -71,27 +71,27 @@ int start_tcp_server(const string &port) {
     return server_sock;
 }
 
-// Function to start a TCP client
+// Function to start a TCP client almost identical to the server (just from the other side ;) )
 int start_tcp_client(const string &hostname, const string &port) {
-    int client_sock = socket(AF_INET, SOCK_STREAM, 0);
+    int client_sock = socket(AF_INET, SOCK_STREAM, 0);// create a socket if error handle it
     if (client_sock < 0) {
         perror("Error creating socket");
         exit(EXIT_FAILURE);
     }
 
-    struct hostent *server = gethostbyname(hostname.c_str());
+    struct hostent *server = gethostbyname(hostname.c_str());   // get the host by name if error handle it
     if (server == nullptr) {
         cerr << "Error: No such host" << endl;
         close(client_sock);
         exit(EXIT_FAILURE);
     }
-
+    // set the server options
     struct sockaddr_in server_addr = {};
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(stoi(port));
-    memcpy(&server_addr.sin_addr.s_addr, server->h_addr, server->h_length);
+    memcpy(&server_addr.sin_addr.s_addr, server->h_addr, server->h_length);// copy the address
 
-    if (connect(client_sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+    if (connect(client_sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {// connect to the server if error handle it
         perror("Error connecting to server");
         close(client_sock);
         exit(EXIT_FAILURE);
@@ -100,7 +100,7 @@ int start_tcp_client(const string &hostname, const string &port) {
     return client_sock;
 }
 
-void signal_handler(int signal) {
+void signal_handler(int signal) {// handle the signals
     if (signal == SIGINT) {
         running = false;
     }
@@ -134,8 +134,6 @@ int main(int argc, char *argv[]) {
                 return EXIT_FAILURE;
         }
     }
-    cout << "the input was: " << input_redirect << endl;
-    cout << "the output was: " << output_redirect << endl;
 
     if (!program) { // If the program name is not provided, print usage and exit
         cerr << "Usage: " << argv[0] << " -e <program> [args] [-i <input_redirect>] [-o <output_redirect>] [-b <bi_redirect>]" << endl;
@@ -159,31 +157,32 @@ int main(int argc, char *argv[]) {
     }
 
     if (pid == 0) { // Child process
-        if (!input_redirect.empty()) {
+        if (!input_redirect.empty()) {// if there is an input redirection
             if (input_redirect.substr(0, 4) == "TCPS") {
-                int port = stoi(input_redirect.substr(4));
+                int port = stoi(input_redirect.substr(4));// get the port
                 cout << "the port was " << port << endl;
-                int server_sock = start_tcp_server(to_string(port));
-                int client_sock = accept(server_sock, nullptr, nullptr);
+                int server_sock = start_tcp_server(to_string(port));// start the server
+                int client_sock = accept(server_sock, nullptr, nullptr);// accept the connection if error handle it
                 if (client_sock < 0) {
                     perror("Error accepting connection");
                     close(server_sock);
                     return EXIT_FAILURE;
                 }
-                handle_client_input(client_sock);
+                handle_client_input(client_sock);// handle the client input if error handle it
                 if (input_redirect == output_redirect) {
                     handle_client_output(client_sock);
                 }
                 close(server_sock);
             }
-            // Additional input redirect implementations can be added here
+            
         }
-
+        // if there is an output redirection handle it (almost identical to the input redirection, only here we have both TCPC and TCPS)
         if (!output_redirect.empty() && output_redirect != input_redirect) {
             if (output_redirect.substr(0, 4) == "TCPS") {
                 cout << "the output redirect was " << output_redirect << endl;
                 int port = stoi(output_redirect.substr(4));
                 int server_sock = start_tcp_server(to_string(port));
+                 std::cerr << "Attempting to accept TCP output on port " << port << "...\n";
                 int client_sock = accept(server_sock, nullptr, nullptr);
                 if (client_sock < 0) {
                     perror("Error accepting connection");
@@ -206,7 +205,6 @@ int main(int argc, char *argv[]) {
                     return EXIT_FAILURE;
                 }
             }
-            // Additional output redirect implementations can be added here
         }
 
         // Set the buffer to be line-buffered
@@ -219,11 +217,12 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     } else { // Parent process
         int status;
-        while (running && waitpid(pid, &status, WNOHANG) == 0) {
+        while (running && waitpid(pid, &status, WNOHANG) == 0) {// wait for the child process to finish
             sleep(1);
         }
-        if (!running) {
+        if (!running) { // If the program was terminated by Ctrl+C, kill the child process
             kill(pid, SIGTERM);
+            return EXIT_FAILURE;
         }
     }
 
