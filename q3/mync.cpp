a9e12dcf5383e1b20436f00f7c75c0841b956988
urikialy <uri.kialy@msmail.ariel.ac.il>
2024-05-26 19:18:36 +0300

    updates

--- q3/mync.cpp
+++ q3/mync.cpp
@@ -11,6 +11,7 @@
 #include <sstream>
 #include <csignal>
 #include <sys/wait.h>
+
 using namespace std;
 bool running = true;
 
@@ -27,15 +28,12 @@ vector<string> split(const string &str) {
 // Function to handle a client connection for input redirection
 void handle_client_input(int client_sock) {
     dup2(client_sock, STDIN_FILENO);
-    close(client_sock);
-    return;
 }
 
 // Function to handle a client connection for output redirection
 void handle_client_output(int client_sock) {
     dup2(client_sock, STDOUT_FILENO);
-    close(client_sock);
-    return;
+    dup2(client_sock, STDERR_FILENO); // Also redirect stderr to the client socket
 }
 
 // Function to start a TCP server
@@ -53,18 +51,18 @@ int start_tcp_server(const string &port) {
         exit(EXIT_FAILURE);
     }
 
-    struct sockaddr_in server_addr = {};// sockaddr_in is a structure containing an internet address
+    struct sockaddr_in server_addr = {};
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(stoi(port));
     server_addr.sin_addr.s_addr = INADDR_ANY;
 
-    if (bind(server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {//try to bind if not successful then print error
+    if (bind(server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
         perror("Error binding socket");
         close(server_sock);
         exit(EXIT_FAILURE);
     }
 
-    if (listen(server_sock, 1) < 0) { 
+    if (listen(server_sock, 1) < 0) {
         perror("Error listening on socket");
         close(server_sock);
         exit(EXIT_FAILURE);
@@ -94,7 +92,7 @@ int start_tcp_client(const string &hostname, const string &port) {
     memcpy(&server_addr.sin_addr.s_addr, server->h_addr, server->h_length);
 
     if (connect(client_sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
-        perror("line 97:Error connecting to server");
+        perror("Error connecting to server");
         close(client_sock);
         exit(EXIT_FAILURE);
     }
@@ -102,7 +100,7 @@ int start_tcp_client(const string &hostname, const string &port) {
     return client_sock;
 }
 
-void signal_handler(int signal) {//signal handler for SIGINT
+void signal_handler(int signal) {
     if (signal == SIGINT) {
         running = false;
     }
@@ -116,7 +114,7 @@ int main(int argc, char *argv[]) {
     string input_redirect, output_redirect;
 
     // Using getopt to parse the command-line arguments
-    while ((opt = getopt(argc, argv, "e:i:o:b:")) != -1) {// the options are e,i,o,b else return error
+    while ((opt = getopt(argc, argv, "e:i:o:b:")) != -1) {
         switch (opt) {
             case 'e':
                 program = optarg;
@@ -132,15 +130,15 @@ int main(int argc, char *argv[]) {
                 output_redirect = optarg;
                 break;
             default:
-                cerr << "Usage: " << argv[0] << "line 135 -e <program> [args] [-i <input_redirect>] [-o <output_redirect>] [-b <bi_redirect>]" << endl;
+                cerr << "Usage: " << argv[0] << " -e <program> [args] [-i <input_redirect>] [-o <output_redirect>] [-b <bi_redirect>]" << endl;
                 return EXIT_FAILURE;
         }
     }
-    cout<<"the input was:"<<input_redirect<<endl;
-    cout<<"the output was:"<<output_redirect<<endl;
+    cout << "the input was: " << input_redirect << endl;
+    cout << "the output was: " << output_redirect << endl;
 
     if (!program) { // If the program name is not provided, print usage and exit
-        cerr << "Usage: " << argv[0] << " line 141 -e <program> [args] [-i <input_redirect>] [-o <output_redirect>] [-b <bi_redirect>]" << endl;
+        cerr << "Usage: " << argv[0] << " -e <program> [args] [-i <input_redirect>] [-o <output_redirect>] [-b <bi_redirect>]" << endl;
         return EXIT_FAILURE;
     }
     
@@ -164,23 +162,26 @@ int main(int argc, char *argv[]) {
         if (!input_redirect.empty()) {
             if (input_redirect.substr(0, 4) == "TCPS") {
                 int port = stoi(input_redirect.substr(4));
-                cout<<"the port was"<<port<<endl;
+                cout << "the port was " << port << endl;
                 int server_sock = start_tcp_server(to_string(port));
                 int client_sock = accept(server_sock, nullptr, nullptr);
                 if (client_sock < 0) {
-                    perror(" line 169 :Error accepting connection");
+                    perror("Error accepting connection");
                     close(server_sock);
                     return EXIT_FAILURE;
                 }
                 handle_client_input(client_sock);
+                if (input_redirect == output_redirect) {
+                    handle_client_output(client_sock);
+                }
                 close(server_sock);
             }
             // Additional input redirect implementations can be added here
         }
 
-        if (!output_redirect.empty()) {
+        if (!output_redirect.empty() && output_redirect != input_redirect) {
             if (output_redirect.substr(0, 4) == "TCPS") {
-                cout<<"the output redirect was"<<output_redirect<<endl;
+                cout << "the output redirect was " << output_redirect << endl;
                 int port = stoi(output_redirect.substr(4));
                 int server_sock = start_tcp_server(to_string(port));
                 int client_sock = accept(server_sock, nullptr, nullptr);
@@ -197,7 +198,7 @@ int main(int argc, char *argv[]) {
                 if (comma_pos != string::npos) {
                     string hostname = host_port.substr(0, comma_pos);
                     string port = host_port.substr(comma_pos + 1);
-                    cout<<"starting tcp with "<<hostname<<" and port "<<port<<endl;
+                    cout << "starting tcp with " << hostname << " and port " << port << endl;
                     int client_sock = start_tcp_client(hostname, port);
                     handle_client_output(client_sock);
                 } else {
@@ -228,4 +229,3 @@ int main(int argc, char *argv[]) {
 
     return 0;
 }
-
