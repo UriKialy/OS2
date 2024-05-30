67792019e4fe2e4b2f7e671df6cfc2f2c121ceda
hila1999 <hila.shamir99@gmail.com>
2024-05-30 10:04:38 +0300

    upload q4

---
+++ q4/mync.cpp
@@ -0,0 +1,334 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <unistd.h>
+#include <getopt.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <sstream>
+#include <csignal>
+#include <sys/wait.h>
+
+using namespace std;
+bool running = true;
+
+// Function to split a string by spaces into a vector of strings
+vector<string> split(const string &str) {
+    vector<string> result;
+    istringstream iss(str);
+    for (string s; iss >> s;) {
+        result.push_back(s);
+    }
+    return result;
+}
+
+// Function to handle a client connection for input redirection
+void handle_client_input(int client_sock) {
+    dup2(client_sock, STDIN_FILENO);
+}
+
+// Function to handle a client connection for output redirection
+void handle_client_output(int client_sock) {
+    dup2(client_sock, STDOUT_FILENO);
+    dup2(client_sock, STDERR_FILENO); // Also redirect stderr to the client socket
+}
+
+// Function to start a TCP server
+int start_tcp_server(const string &port) {
+    int server_sock = socket(AF_INET, SOCK_STREAM, 0);// create a socket if error handle it
+    if (server_sock < 0) {// handle error
+        perror("Error creating socket");
+        exit(EXIT_FAILURE);
+    }
+
+    int opt = 1;
+    if (setsockopt(server_sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {// set socket options if error handle it
+        perror("Error setting socket options");
+        close(server_sock);
+        exit(EXIT_FAILURE);
+    }
+    // set the server options
+    struct sockaddr_in server_addr = {};
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(stoi(port));
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) { // bind the socket if error handle it
+        perror("Error binding socket");
+        close(server_sock);
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_sock, 1) < 0) { // listen on the socket if error handle it
+        perror("Error listening on socket");
+        close(server_sock);
+        exit(EXIT_FAILURE);
+    }
+
+    return server_sock;
+}
+
+// Function to start a TCP client almost identical to the server (just from the other side ;) )
+int start_tcp_client(const string &hostname, const string &port) {
+    int client_sock = socket(AF_INET, SOCK_STREAM, 0);// create a socket if error handle it
+    if (client_sock < 0) {
+        perror("Error creating socket");
+        exit(EXIT_FAILURE);
+    }
+
+    struct hostent *server = gethostbyname(hostname.c_str());   // get the host by name if error handle it
+    if (server == nullptr) {
+        cerr << "Error: No such host" << endl;
+        close(client_sock);
+        exit(EXIT_FAILURE);
+    }
+    // set the server options
+    struct sockaddr_in server_addr = {};
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(stoi(port));
+    memcpy(&server_addr.sin_addr.s_addr, server->h_addr, server->h_length);// copy the address
+
+    if (connect(client_sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {// connect to the server if error handle it
+        perror("Error connecting to server");
+        close(client_sock);
+        exit(EXIT_FAILURE);
+    }
+
+    return client_sock;
+}
+
+// Function to start a UDP server
+int start_udp_server(const string &port) {
+    int server_sock = socket(AF_INET, SOCK_DGRAM, 0); // create a socket if error handle it
+    if (server_sock < 0) { // handle error
+        perror("Error creating socket");
+        exit(EXIT_FAILURE);
+    }
+
+    struct sockaddr_in server_addr = {};
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(stoi(port));
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) { // bind the socket if error handle it
+        perror("Error binding socket");
+        close(server_sock);
+        exit(EXIT_FAILURE);
+    }
+
+    return server_sock;
+}
+
+// Function to start a UDP client
+int start_udp_client(const string &hostname, const string &port) {
+    int client_sock = socket(AF_INET, SOCK_DGRAM, 0); // create a socket if error handle it
+    if (client_sock < 0) {
+        perror("Error creating socket");
+        exit(EXIT_FAILURE);
+    }
+
+    struct hostent *server = gethostbyname(hostname.c_str()); // get the host by name if error handle it
+    if (server == nullptr) {
+        cerr << "Error: No such host" << endl;
+        close(client_sock);
+        exit(EXIT_FAILURE);
+    }
+    // set the server options
+    struct sockaddr_in server_addr = {};
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(stoi(port));
+    memcpy(&server_addr.sin_addr.s_addr, server->h_addr, server->h_length); // copy the address
+
+    // Note: No need to connect for UDP client
+
+    return client_sock;
+}
+// Function to handle timeout
+void handle_timeout(int signal) {
+    cout << "Timeout reached. Killing processes..." << endl;
+    running = false;
+}
+
+void signal_handler(int signal) {// handle the signals
+    if (signal == SIGINT) {
+        running = false;
+    }
+}
+
+int main(int argc, char *argv[]) {
+    signal(SIGINT, signal_handler); // Handle Ctrl+C to terminate the program gracefully
+    signal(SIGALRM, handle_timeout); // Handle alarm signal for timeout
+    int opt;
+    char *program = nullptr;
+    string input_redirect, output_redirect, timeout;
+    bool udp_server = false;
+    bool udp_client = false;
+
+
+    // Using getopt to parse the command-line arguments
+    while ((opt = getopt(argc, argv, "e:i:o:b:t:")) != -1) {
+        switch (opt) {
+            case 'e':
+                program = optarg;
+                break;
+            case 'i':
+                input_redirect = optarg;
+                if (input_redirect.substr(0, 4) == "UDPS") {
+                    udp_server = true;
+                     // Extract port from input_redirect
+                    string port = input_redirect.substr(4);
+                    cout << "Starting UDP server on port " << port << endl;
+                    int server_sock = start_udp_server(port);
+                    // Set alarm for timeout
+                    if (!timeout.empty()) {
+                        alarm(stoi(timeout));
+                    }
+                    // handle_client_input for UDP server
+                    handle_client_input(server_sock);
+                    udp_server = true;
+                }
+                break;
+            case 'o':
+                output_redirect = optarg;
+                 if (output_redirect.substr(0, 4) == "UDPC") {
+                    // Extract hostname and port from output_redirect
+                    string host_port = output_redirect.substr(4);
+                    size_t comma_pos = host_port.find(',');
+                    if (comma_pos != string::npos) {
+                        string hostname = host_port.substr(0, comma_pos);
+                        string port = host_port.substr(comma_pos + 1);
+                        cout << "Starting UDP client with " << hostname << " and port " << port << endl;
+                        int client_sock = start_udp_client(hostname, port);
+                        // handle_client_output for UDP client
+                        handle_client_output(client_sock);
+                        udp_client = true;
+                    } else {
+                        cerr << "Invalid UDPC format. Expected UDPC<hostname,port>" << endl;
+                        return EXIT_FAILURE;
+                    }
+                 }
+                break;
+            case 'b':
+                input_redirect = optarg;
+                output_redirect = optarg;
+                break;
+            case 't':
+                timeout = optarg;
+                break;
+            default:
+                cerr << "Usage: " << argv[0] << " -e <program> [args] [-i <input_redirect>] [-o <output_redirect>] [-b <bi_redirect>]" << endl;
+                return EXIT_FAILURE;
+        }
+    }
+
+    if (!program) { // If the program name is not provided, print usage and exit
+        cerr << "Usage: " << argv[0] << " -e <program> [args] [-i <input_redirect>] [-o <output_redirect>] [-b <bi_redirect>]" << endl;
+        return EXIT_FAILURE;
+    }
+    
+    // Add "./" to the beginning of the program name
+    string program_str = "./" + string(program);
+
+    vector<string> split_program = split(program_str); // Split the program name and arguments
+    vector<char*> args; // Vector of char* to store the arguments for execvp
+    for (const auto &arg : split_program) { // Convert the arguments to char* and store in the vector
+        args.push_back(const_cast<char*>(arg.c_str()));
+    }
+    args.push_back(nullptr);
+
+    pid_t pid = fork();
+    if (pid < 0) {
+        perror("Error forking process"); // If fork fails, print error and exit
+        return EXIT_FAILURE;
+    }
+
+    if (pid == 0) { // Child process
+         if (udp_server && udp_client) {
+            cerr << "Cannot have both UDP server and client in the same process" << endl;
+            return EXIT_FAILURE;
+        }
+        if (!input_redirect.empty()) {// if there is an input redirection
+            if (input_redirect.substr(0, 4) == "TCPS") {
+                int port = stoi(input_redirect.substr(4));// get the port
+                cout << "the port was " << port << endl;
+                int server_sock = start_tcp_server(to_string(port));// start the server
+                int client_sock = accept(server_sock, nullptr, nullptr);// accept the connection if error handle it
+                if (client_sock < 0) {
+                    perror("Error accepting connection");
+                    close(server_sock);
+                    return EXIT_FAILURE;
+                }
+                handle_client_input(client_sock);// handle the client input if error handle it
+                if (input_redirect == output_redirect) {
+                    handle_client_output(client_sock);
+                }
+                close(server_sock);
+            }
+            
+        }
+        // if there is an output redirection handle it (almost identical to the input redirection, only here we have both TCPC and TCPS)
+        if (!output_redirect.empty() && output_redirect != input_redirect) {
+            if (output_redirect.substr(0, 4) == "TCPS") {
+                cout << "the output redirect was " << output_redirect << endl;
+                int port = stoi(output_redirect.substr(4));
+                int server_sock = start_tcp_server(to_string(port));
+                 std::cerr << "Attempting to accept TCP output on port " << port << "...\n";
+                int client_sock = accept(server_sock, nullptr, nullptr);
+                if (client_sock < 0) {
+                    perror("Error accepting connection");
+                    close(server_sock);
+                    return EXIT_FAILURE;
+                }
+                handle_client_output(client_sock);
+                close(server_sock);
+            } else if (output_redirect.substr(0, 4) == "TCPC") {
+                string host_port = output_redirect.substr(4);
+                size_t comma_pos = host_port.find(',');
+                if (comma_pos != string::npos) {
+                    string hostname = host_port.substr(0, comma_pos);
+                    string port = host_port.substr(comma_pos + 1);
+                    cout << "starting tcp with " << hostname << " and port " << port << endl;
+                    int client_sock = start_tcp_client(hostname, port);
+                    handle_client_output(client_sock);
+                } else {
+                    cerr << "Invalid TCPC format. Expected TCPC<hostname,port>" << endl;
+                    return EXIT_FAILURE;
+                }
+            }
+        }
+         if (!timeout.empty()) {
+            // Set the alarm for timeout
+            alarm(stoi(timeout));
+        }
+        // Set the buffer to be line-buffered
+        setvbuf(stdout, nullptr, _IOLBF, BUFSIZ);
+
+        // Executing the specified program with the given arguments
+        execvp(args[0], args.data());
+        // execvp only returns if an error occurs
+        perror("Error executing program");
+        return EXIT_FAILURE;
+    } else { // Parent process
+        int status;
+         if (!timeout.empty()) {
+            // Wait for the child process to finish or timeout
+            while (running && waitpid(pid, &status, WNOHANG) == 0) {
+                sleep(1);
+            }
+            if (running) {
+                cout << "Timeout reached. Killing child process..." << endl;
+                kill(pid, SIGKILL);
+            }
+        } else {
+            // Wait for the child process to finish
+            while (running && waitpid(pid, &status, 0) == 0) {
+                sleep(1);
+            }
+        }
+    }
+
+    return 0;
+}
---
+++ q4/ttt.cpp
@@ -0,0 +1,119 @@
+#include "ttt.hpp"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstring>
+#include <limits>
+
+
+#define BOARD_SIZE 9
+
+void print_board(const std::vector<char>& board) {
+    for (int i = 0; i < BOARD_SIZE; ++i) {
+        if (i % 3 == 0 && i != 0)
+            std::cout << std::endl;
+        std::cout << "[" << board[i] << "] ";
+    }
+    std::cout << std::endl;
+}
+
+bool check_win(const std::vector<char>& board, char player) {
+    std::vector<std::vector<int>> win_positions = {
+        {0, 1, 2}, {3, 4, 5}, {6, 7, 8},  // Rows
+        {0, 3, 6}, {1, 4, 7}, {2, 5, 8},  // Columns
+        {0, 4, 8}, {2, 4, 6}              // Diagonals
+    };
+    for (const auto& win_pos : win_positions) {
+        if (board[win_pos[0]] == player &&
+            board[win_pos[1]] == player &&
+            board[win_pos[2]] == player) {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2 || strlen(argv[1]) != 9) {
+        std::cout << "Error" << std::endl;
+        return 1;
+    }
+
+    std::string strategy = argv[1];
+
+    for (char ch = '1'; ch <= '9'; ++ch) {
+        if (strategy.find(ch) == std::string::npos) {
+            std::cout << "Error" << std::endl;
+            return 1;
+        }
+    }
+
+    std::vector<char> board(BOARD_SIZE, '-');
+    int move, played_moves = 0;
+    char player = 'X', opponent = 'O';
+
+    while (true) {
+        // Program's move
+        if (played_moves < 8) {
+            for (int i = 0; i < 9; ++i) {
+                int pos = strategy[i] - '1';
+                if (board[pos] != 'X' && board[pos] != 'O') {
+                    board[pos] = player;
+                    std::cout << pos + 1 << std::endl;
+                    break;
+                }
+            }
+        } else {
+            for (int i = 8; i >= 0; --i) {
+                int pos = strategy[i] - '1';
+                if (board[pos] != 'X' && board[pos] != 'O') {
+                    board[pos] = player;
+                    std::cout << pos + 1 << std::endl;
+                    break;
+                }
+            }
+        }
+
+        played_moves++;
+        print_board(board);
+
+        if (check_win(board, player)) {
+            std::cout << "I win" << std::endl;
+            return 0;
+        }
+
+        if (played_moves == 9) {
+            std::cout << "DRAW" << std::endl;
+            return 0;
+        }
+
+        // Player's move
+        std::cout << "Enter your move: \n";
+        while (true) {
+            if (!(std::cin >> move) || move < 1 || move > 9 || board[move - 1] == 'X' || board[move - 1] == 'O') {
+                std::cout << "Invalid move. Enter a number between 1 and 9: ";
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            } else {
+                break;
+            }
+        }
+        
+        board[move - 1] = opponent;
+
+        played_moves++;
+        print_board(board);
+
+        if (check_win(board, opponent)) {
+            std::cout << "I lost" << std::endl;
+            return 0;
+        }
+
+        if (played_moves == 9) {
+            std::cout << "DRAW" << std::endl;
+            return 0;
+        }
+    }
+
+    return 0;
+}
